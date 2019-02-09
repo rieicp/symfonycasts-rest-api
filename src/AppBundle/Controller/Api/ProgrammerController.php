36b2cc720210d502c814a0666a34ce9fb421cad4
Ning Wei <ning.wei@int-trade.de>
2019-02-09 23:30:16 +0100

    create a new programmer according to posted data and persist into DB -- Attention: if PDOException thrown with 'could not find driver' error, try 'sudo apt install php7.0-mysql' and then restart the built-in server. == command 'php --ini | grep pdo' should list following ini files:    >> xxx/.../cli/xxx/.../pdo.ini    >> xxx/.../cli/xxx/.../pdo_mysql.ini

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -2,13 +2,14 @@
 
 namespace AppBundle\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Controller\BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Programmer;
 
-class ProgrammerController extends Controller
+class ProgrammerController extends BaseController
 {
    /**
 	* @Route("/api/programmers")
@@ -16,7 +17,16 @@ class ProgrammerController extends Controller
 	*/
 	public function newAction(Request $request)
 	{
-		$data = $request->getContent();
-		return new Response($data);
+
+		$data = json_decode($request->getContent(), true);
+		$programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+		$programmer->setTagLine($data['tagLine']);
+		$programmer->setUser($this->findUserByUsername('weaverryan'));
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($programmer);
+		$em->flush();
+
+		return new Response('It worked. Believe me - I\'m an API');
 	}
 }

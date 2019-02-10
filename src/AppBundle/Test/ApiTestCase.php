226bc6ee6fa6ebc8b543509c72b58f51c715b5dc
Ning Wei <ning.wei@int-trade.de>
2019-02-10 19:40:33 +0100

    输出彩色的错误信息：use ConsoleOutput to print error infomation with color! 错误信息下方也列出了profiler的地址，访问该地址，如：    Profiler URL: localhost:8000/_profiler/8da56b 在Exception栏中可以清晰看到错误信息，以及堆栈的分析

--- src/AppBundle/Test/ApiTestCase.php
+++ src/AppBundle/Test/ApiTestCase.php
@@ -28,6 +28,11 @@ class ApiTestCase extends KernelTestCase
     protected $client;
 
     /**
+     * @var ConsoleOutput
+     */     
+    private $output;
+
+    /**
      * @var FormatterHelper
      */
     private $formatterHelper;
@@ -194,7 +199,11 @@ class ApiTestCase extends KernelTestCase
      */
     protected function printDebug($string)
     {
-        echo $string."\n";
+        if ($this->output === null) {
+           $this->output = new ConsoleOutput();
+        }
+        
+        $this->output->writeln($string);
     }
 
     /**
